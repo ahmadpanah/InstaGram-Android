1a7016110c6d3327e557e7938b40e36f6bdad416
Hossein <djalicrt@gmail.com>
2020-09-28 17:30:54 +0330

    Second Part - Design Login Layout With Animation

--- app/src/main/java/ir/shariaty/instagram/StartActivity.java
+++ app/src/main/java/ir/shariaty/instagram/StartActivity.java
@@ -3,12 +3,60 @@ package ir.shariaty.instagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class StartActivity extends AppCompatActivity {
+
+    private ImageView iconImage;
+    private LinearLayout linearLayout;
+    private Button register;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        iconImage = findViewById(R.id.icon_image);
+        linearLayout = findViewById(R.id.linear_layout);
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
+
+        linearLayout.animate().alpha(0f).setDuration(1);
+
+        TranslateAnimation animation = new TranslateAnimation(0, 0, 0,  -1000);
+        animation.setDuration(1000);
+        animation.setFillAfter(false);
+        animation.setAnimationListener (new MyAnimationListner());
+
+        iconImage.setAnimation(animation);
+
+    }
+
+    private class MyAnimationListner implements Animation.AnimationListener {
+        @Override
+        public void onAnimationStart (Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+
+            iconImage.clearAnimation();
+            iconImage.setVisibility(View.INVISIBLE);
+            linearLayout.animate().alpha(1f).setDuration(1000);
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+
     }
 }
